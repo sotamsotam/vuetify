743218fcfe4167d1e45f86a79a8c3c0445549eb3
jacocho858 <jacocho858@gmail.com>
2022-07-13 11:11:50 +0900

    vuetify data table master

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <v-app>
+      <v-app-bar app dark>
+        <div class="d-flex align-center mr-2">bezKoder</div>
+        <v-btn to="/tutorials" text> Tutorials </v-btn>
+        <v-btn to="/add" text> Add </v-btn>
+      </v-app-bar>
+      <v-main>
+        <router-view />
+      </v-main>
+    </v-app>
+  </div>
+</template>
+<script>
+export default {
+  name: "app",
+};
+</script>
